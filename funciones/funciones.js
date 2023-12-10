c9a49e7d16aca1d42a339e7bc5d00c3d9c8bc219
EdwardQuevedoP <edwardquevedop@gmail.com>
2023-12-10 14:40:18 -0500

    0.5

---
+++ funciones/funciones.js
@@ -0,0 +1,10 @@
+//las funciones son codigos que se pueden reutilizar en varias ocaciones
+//a continuacion vamos a hacer una funcion para contar
+function suma (num1,num2){
+    return num1+num2
+    //lo que estamos haciendo es crear una funcion que se le adquiere dos datos
+    //esos datos dice que los vamos a retornar y los sumamos 
+}
+var resultado = suma(5,10)//creamos una variable que se llame resultado que llame la funcion y le de valor a los dos datos 
+console.log(resultado)//imprimimos la variable resultado
+// imprime  15
